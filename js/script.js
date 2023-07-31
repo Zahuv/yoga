2aacd3dfa53d1bf07267b829f27c1b246c711ea2
Zahuv <arinbergvibe@gmail.com>
2023-07-31 06:05:12 -0700

    New timer format added(00days:00hours:00minutes:00seconds)

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    info.addEventListener('click', function(event) {                            //НАВЕШИВАЕМ ОБРАБОТЧИК СОБЫТИЯ. ПОИЕЩАЕМ ОБЬЕКТ В ФУНКЦИЮ Т.К. БУДЕМ СРАВНИВАТЬ С ТЕМ, КУДА МЫ КЛИКАЕМ
+    info.addEventListener('click', function(event) {                            //НАВЕШИВАЕМ ОБРАБОТЧИК СОБЫТИЯ. ПОМЕЩАЕМ ОБЬЕКТ В ФУНКЦИЮ Т.К. БУДЕМ СРАВНИВАТЬ С ТЕМ, КУДА МЫ КЛИКАЕМ
         let target = event.target;                                              //ЗАДАЕМ ПЕРЕМЕННУЮ ДЛЯ СОБЫТИЯ                                 
         if (target && target.classList.contains('info-header-tab')) {           //ПРОВЕРЯЕМ ЧТО МЫ ДЕЙСТВИТЕЛЬНО КЛИКНУЛИ НА ЭЛЕМЕНТ
             for (let i = 0; i < tab.length; i++) {                              // ЗАПУСКАЕМ ЦИКЛ ПЕРЕБОРА, ЧТОБЫ ВЫБРАТЬ НУЖНЫЙ ТАБ
@@ -36,19 +36,22 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //TIMER
 
-    let deadline = '2023-09-21';                                    //СТАВИМ ДАТУ ЗАВЕРШЕНИЯ РАБОТЫ ТАЙМЕРА 
+    let deadline = '2023-09-10';                                                        //СТАВИМ ДАТУ ЗАВЕРШЕНИЯ РАБОТЫ ТАЙМЕРА 
 
-    function getTimeRemaining(endtime) {                            //СОЗДАЕМ ФУНКЦИЮ КОТОРАЯ БУДЕТ ОПРЕДЕЛЯТЬ СКОЛЬКО ВРЕМЕНИ ОСТАЛОСЬ ОТ НЫНЕШНЕГО МОМЕНТА ДО ДЕДЛАЙНА
-        let t = Date.parse(endtime) - Date.parse(new Date()),       //СОЗДАЕМ ПЕРЕМЕННУЮ В КОТОРУЮ БУДЕМ ЗАПИСЫВАТЬ РАЗНИЦУ МЕЖДУ ДЕДЛАЙНОМ И НЫНЕШНЕЙ ДАТОЙ
-        seconds = Math.floor((t/1000) % 60),                        //ВЫСЧИТЫВАЕМ СЕКУНДЫ
-        minutes = Math.floor((t/1000/60) % 60),                     //ВЫСЧИТЫВАЕМ МИНУТЫ
-        hours = Math.floor((t/(1000*60*60)));                       //ВЫСЧИТЫВАЕМ ЧАСЫ
+    function getTimeRemaining(endtime) {                                                //СОЗДАЕМ ФУНКЦИЮ КОТОРАЯ БУДЕТ ОПРЕДЕЛЯТЬ СКОЛЬКО ВРЕМЕНИ ОСТАЛОСЬ ОТ НЫНЕШНЕГО МОМЕНТА ДО ДЕДЛАЙНА
+        let t = Date.parse(endtime) - Date.parse(new Date()),         //СОЗДАЕМ ПЕРЕМЕННУЮ В КОТОРУЮ БУДЕМ ЗАПИСЫВАТЬ РАЗНИЦУ МЕЖДУ ДЕДЛАЙНОМ И НЫНЕШНЕЙ ДАТОЙ
+        seconds = Math.floor((t/1000) % 60),                                            //ВЫСЧИТЫВАЕМ СЕКУНДЫ
+        minutes = Math.floor((t/1000/60) % 60),                                         //ВЫСЧИТЫВАЕМ МИНУТЫ
+        hours = Math.floor((t/1000/60/60) % 24),                                        //ВЫСЧИТЫВАЕМ ЧАСЫ
+        days = Math.floor((t/(1000*60*60*24)));                                         //ВЫСЧИТЫВАЕМ ДНИ
 
+        //hours = Math.floor((t/(1000*60*60)))
         //hours = Math.floor((t/1000/60/60) % 24);                  //ВЫСЧИТЫВАЕМ ЧАСЫ В 24-Х ЧАСОВОМ ФОРМАТЕ
         //days = Math.floor((t/(1000*60*60*24)));                   //ВЫСЧИТЫВАЕМ ДНИ
 
         return {                                                    //ПРОПИСЫВАЕМ ЧТО ФУНКЦИЯ ДОЛЖНА ВОЗВРАЩАТЬ
             'total' : t,
+            'days'  : days,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
@@ -57,6 +60,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function setClock(id, endtime) {                                           //СОЗДАЕМ ФУНКЦИЮ, ДЕЛАЮЩУЮ ВЕРСТКУ ДИНАМИЧНОЙ, ПЕРЕДАЕМ ПАРАМЕТРЫ ID БЛОКА ГДЕ СВЕРСТАН ТАЙМЕР И ДЕДЛАЙН
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -64,12 +68,24 @@ window.addEventListener('DOMContentLoaded', function() {
 
         function updateClock() {                                        //СОЗДАЕМ ФУНКЦИЮ КОТОРАЯ КАЖДУЮ СЕКУНДУ БУДЕТ ОБНОВЛЯТЬ ДАННЫЕ В ТАЙМЕРЕ
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;                                
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            function addZero(num) {
+                        if (num <= 9) {
+                            return '0' + num;
+                        } else return num
+                    };
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);                                
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             } 
         }
 
